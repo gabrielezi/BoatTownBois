0e465af06010e70158d4799a3c84b9b7ec59e7d6
juliusv <juliusvaliukas@gmail.com>
2020-11-18 18:47:45 +0200

    Build sound

--- Assets/Scripts/Player/PlayerBuild.cs
+++ Assets/Scripts/Player/PlayerBuild.cs
@@ -6,7 +6,15 @@ public class PlayerBuild : MonoBehaviour
 {
     [SerializeField]
     private GameObject _building;
+    [SerializeField]
+    private GameObject _slider;
     // Start is called before the first frame update
+
+    private bool _isBuilding = false;
+    private const float defaultBuildTime = 3; 
+    private float _buildTimeRemaining = defaultBuildTime;
+    private Vector3 _nextBuildPosition;
+
     void Start()
     {
         
@@ -15,10 +23,28 @@ public class PlayerBuild : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("b"))
+
+        if (Input.GetKeyDown("b") && !_isBuilding)
         {
+            SoundManager.Instance.PlaySound("Build");
+            //Instantiate(_slider, _nextBuildPosition, Quaternion.identity);
             //todo: use resources
-            Instantiate(_building, transform.position, Quaternion.identity);
+            _isBuilding = true;
+            _nextBuildPosition = transform.position;
+        }
+        if (_isBuilding)
+        {
+            if(_buildTimeRemaining > 0)
+            {
+                _buildTimeRemaining -= Time.deltaTime;
+            } 
+            else
+            {
+                Instantiate(_building, _nextBuildPosition, Quaternion.identity);
+                SoundManager.Instance.StopSound("Build");
+                _isBuilding = false;
+                _buildTimeRemaining = defaultBuildTime;
+            }
         }
     }
 }
